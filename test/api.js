4a33f5f07b280726746df87e102ad323e31ac1ea
Thaddee Tyl <thaddee.tyl@gmail.com>
2015-06-17 00:24:37 +0200

    Testing email verification from the wrong device

--- test/api.js
+++ test/api.js
@@ -5,7 +5,7 @@ var Api = require('../src/api.js');
 var directory = __dirname + '/shadow';
 var api;
 
-var test = function(cb) {
+var normalFlowTest = function(cb) {
   var email = 'thaddee.tyl@example.com';
   api.login(function(err, token) {
     if (err != null) { throw err; }
@@ -13,18 +13,18 @@ var test = function(cb) {
       token: token,
       email: email,
       subject: function() { return '[example] Identity check'; },
-      textMessage: function(linkToken) {
-        return 'Click: https://127.0.0.1/' + linkToken;
+      textMessage: function(emailToken) {
+        return 'Click: https://127.0.0.1/' + emailToken;
       },
-      htmlMessage: function(linkToken) {
-        return 'Click: https://127.0.0.1/' + linkToken;
+      htmlMessage: function(emailToken) {
+        return 'Click: https://127.0.0.1/' + emailToken;
       },
-    }, function(err, linkToken) {
+    }, function(err, emailToken) {
       if (err != null) { throw err; }
 
-      api.confirmEmail(linkToken, function(err, valid, session) {
+      api.confirmEmail(token, emailToken, function(err, token, session) {
         if (err != null) { throw err; }
-        assert(valid, 'Email confirmation should succeed');
+        assert(!!token, 'Email confirmation should succeed');
         assert(session.emailVerified(), 'Email should be verified');
         assert.equal(session.email, email, 'Email should be stored');
 
@@ -33,6 +33,134 @@ var test = function(cb) {
           assert(valid, 'Login authentication should succeed');
           cb();
         });
+      });
+    });
+  });
+};
+
+var wrongConfirmationTest = function(cb) {
+  var email = 'thaddee.tyl@example.com';
+  api.login(function(err, token, session) {
+    if (err != null) { throw err; }
+    api.proveEmail({
+      token: token,
+      email: email,
+      subject: function() { return '[example] Identity check'; },
+      textMessage: function(emailToken) {
+        return 'Click: https://127.0.0.1/' + emailToken;
+      },
+      htmlMessage: function(emailToken) {
+        return 'Click: https://127.0.0.1/' + emailToken;
+      },
+    }, function(err, emailToken) {
+      if (err != null) { throw err; }
+
+      // Check that we cannot authorize an invalid token.
+      var secret = '';
+      for (var i = 0; i < 32; i++) {
+        secret += '0';
+      }
+      emailToken = Api.encodeToken(session.id, secret);
+
+      api.confirmEmail(token, emailToken,
+      function(err, newToken, newSession) {
+        if (err != null) { throw err; }
+        assert(!newToken,
+          'Email confirmation should fail from wrong token');
+        cb();
+      });
+    });
+  });
+};
+
+var unknownDeviceConfirmationTest = function(cb) {
+  var email = 'thaddee.tyl@example.com';
+  api.login(function(err, token, session) {
+    if (err != null) { throw err; }
+    api.proveEmail({
+      token: token,
+      email: email,
+      subject: function() { return '[example] Identity check'; },
+      textMessage: function(emailToken) {
+        return 'Click: https://127.0.0.1/' + emailToken;
+      },
+      htmlMessage: function(emailToken) {
+        return 'Click: https://127.0.0.1/' + emailToken;
+      },
+    }, function(err, emailToken) {
+      if (err != null) { throw err; }
+
+      api.confirmEmail(undefined, emailToken,
+      function(err, newToken, newSession) {
+        if (err != null) { throw err; }
+        assert(!!newToken,
+          'Email confirmation should succeed from unknown device');
+        assert.notEqual(token, newToken,
+          'Email confirmation give distinct token to unknown device');
+        assert(session.emailVerified(),
+          'Email should be verified for known device');
+        assert.equal(session.email, email,
+          'Email should be stored for known device');
+        assert(newSession.emailVerified(),
+          'Email should be verified for unknown device');
+        assert.equal(newSession.email, email,
+          'Email should be stored for unknown device');
+        cb();
+      });
+    });
+  });
+};
+
+var wrongDeviceConfirmationTest = function(cb) {
+  var email = 'thaddee.tyl@example.com';
+  api.login(function(err, token, session) {
+    if (err != null) { throw err; }
+    api.proveEmail({
+      token: token,
+      email: email,
+      subject: function() { return '[example] Identity check'; },
+      textMessage: function(emailToken) {
+        return 'Click: https://127.0.0.1/' + emailToken;
+      },
+      htmlMessage: function(emailToken) {
+        return 'Click: https://127.0.0.1/' + emailToken;
+      },
+    }, function(err, emailToken) {
+      if (err != null) { throw err; }
+
+      // New device, used for confirmation.
+      api.login(function(err, otherToken, otherSession) {
+        if (err != null) { throw err; }
+
+        api.confirmEmail(otherToken, emailToken,
+        function(err, newToken, newSession) {
+          if (err != null) { throw err; }
+          assert(!!newToken,
+            'Email confirmation should succeed from wrong device');
+          assert.notEqual(token, newToken,
+            'Email confirmation give distinct token to wrong device');
+          assert(session.emailVerified(),
+            'Email should be verified for right device');
+          assert.equal(session.email, email,
+            'Email should be stored for right device');
+          assert(newSession.emailVerified(),
+            'Email should be verified for wrong device');
+          assert.equal(newSession.email, email,
+            'Email should be stored for wrong device');
+          cb();
+        });
+      });
+    });
+  });
+};
+
+var test = function(cb) {
+  normalFlowTest(function(err) {
+    wrongConfirmationTest(function(err) {
+      if (err != null) { throw err; }
+      wrongDeviceConfirmationTest(function(err) {
+        if (err != null) { throw err; }
+        unknownDeviceConfirmationTest(cb);
       });
     });
   });
